2cadef8298f6333fcf9e8868b8eead52d841bb28
Jon C <nightydog27@hotmail.com>
2021-02-12 03:36:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ var kittens = ["Milo", "Otis", "Garfield"]
 /function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
+
+/function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+/function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+/function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+/function appendKitten(name) {
+  
+}
+
+/function prependKitten(name) {
+  
+}
